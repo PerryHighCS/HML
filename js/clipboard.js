4f5d4d33977740f94302b6d30fc4a4f5a3dc75a0
dahlem.brian <dahlem.brian@28T-F407-207718.cusd80.lz>
2017-12-09 00:22:07 -0700

    improved clipboard copy

--- js/clipboard.js
+++ js/clipboard.js
@@ -5,6 +5,7 @@ function copyInstructions() {
     let code = "";
     let line = 1;
 
+    
     // Get each program instruction
     $('#program .instruction').each(function () {
         // Make a temporary copy of the instruction
@@ -16,22 +17,64 @@ function copyInstructions() {
         // Add the line number and instruction text to the output code
         code += line + '\t';
         code += (c.contents().text().trim().replace(/\s+/g, ' '));
-        code += '\n';
+        code += '\r\n';
 
         // increment the line number
         line++;
     });
 
-    // Create a temporary text area to copy the code from
-    var $temp = $("<textarea>");
-    $("body").append($temp);
+    let html = $('<table>');
+    let serialized = "";
+    line = 1;
+    
+    // Get each program instruction
+    $('#program .instruction').each(function () {
+        // add a row to the table
+        let row = $('<tr>');
+        html.append(row);
+        
+        // append the line number
+        row.append(($('<th>').text(line)));
+        
+        // Make a temporary copy of the instruction
+        let c = $(this).clone();
 
-    // Add the code to the text area and select it
-    $temp.val(code).select();
+        // Remove the dropdowns from the copy
+        c.find('ul').remove();
 
-    // Copy the code to the clipboard
-    document.execCommand("copy");
-
-    // Remove the temporary text area
-    $temp.remove();
+        // Add the line number and instruction text to the output code
+        row.append($('<td>').text(c.contents().text().trim().replace(/\s+/g, ' ')));
+        
+        // Convert the program instruction into serialized code
+        serialized += instToCode($(this));
+        
+        // increment the line number
+        line++;
+    });
+    
+    let row = $('<tr>');
+    html.prepend(row);
+    
+    td = $('<th>').text("Run:");
+    td.css('border-bottom', '1px solid black');
+    
+    row.append(td);
+    td = $('<td>');
+    td.css('border-bottom', '1px solid black');
+    
+    let p = $('<p>');
+    let a = $('<a>');
+    a.prop("href", window.location);
+    a.text(serialized);
+    td.append(p);
+    row.append(td);
+    p.append(a);
+        
+    console.log(html.prop('outerHTML'));
+    
+    var dt = new clipboard.DT();
+    dt.setData("text/plain", code);
+    dt.setData("text/html", html.prop('outerHTML'));
+    
+    clipboard.write(dt);
 }
